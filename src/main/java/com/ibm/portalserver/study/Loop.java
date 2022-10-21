42b3440530e58ccb9b641805507f11c90064c289
Ze Xu Fang <Ze.Xu.Fang@ibm.com>
2022-10-21 17:28:38 +0800

    同步

--- src/main/java/com/ibm/portalserver/study/Loop.java
+++ src/main/java/com/ibm/portalserver/study/Loop.java
@@ -2,7 +2,9 @@ package com.ibm.portalserver.study;
 
 public class Loop {
     public static void main(String[] args) {
+        int count = 0;
         for(int i=5;i<101;i++){
+            count++;
             if(i%2==0){
                 System.out.println(i);
             }
